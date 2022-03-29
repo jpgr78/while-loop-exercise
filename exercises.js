1eb7994e6dda9c3cf24195ec14061bc76d090a7f
MacMini18 <mcm-mac-18@MacMini18s-Mac-mini.local>
2022-03-29 10:51:36 -0700

    ready

---
+++ exercises.js
@@ -0,0 +1,30 @@
+let i = 0;
+let num=1000
+
+while (i < 9) {
+    i++;
+    
+    j=10-i
+    console.log(i,j)
+}
+i=1000
+while (i >= 10) {
+    i=i/2;
+    
+   
+    console.log(i)
+}
+i=0
+while (i < 12) {
+    i++;
+    
+    if(i!=3)
+    console.log(i)
+}
+i=0
+while (i < 12) {
+    i++;
+    
+    if(!((i==3)||(i==6)))
+    console.log(i)
+}
